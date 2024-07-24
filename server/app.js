40dd61396b5c75294bde34d8cff364bb4e1ed340
Huzaifa246 <huzaifa24680@gmail.com>
2024-07-25 01:21:45 +0500

    Updated login api

--- server/app.js
+++ server/app.js
@@ -42,13 +42,14 @@ app.post('/api/register', async (req, res) => {
     }
 })
 
-app.post('/api/login', async (req, res, next) => {
+app.post('/api/login', async (req, res) => {
     try {
         const { email, password } = req.body;
 
         if (!email || !password) {
             return res.status(400).send('Please fill the required fields');
         }
+
         const user = await Users.findOne({ email });
 
         if (!user) {
@@ -57,31 +58,32 @@ app.post('/api/login', async (req, res, next) => {
 
         const isMatch = await bcrypt.compare(password, user.password);
 
-        if (password !== user.password) {
+        if (!isMatch) {
             return res.status(400).send('Invalid credentials');
         }
-        else {
-            const payload = {
-                userId: user?.id,
-                email: user.email
+
+        const payload = {
+            userId: user.id,
+            email: user.email
+        };
+
+        console.log(payload, 'payload')
+        const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "JWT_SECRET_KEY";
+        
+        jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '84600' }, async (err, token) => {
+            if (err) {
+                return res.status(500).send('Error generating token');
             }
-            const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "JWT_SECRET_KEY"
-            jwt.sign(payload, JWT_SECRET_KEY, {
-                expiresIn: '84600'
-            }, async (err, token) => {
-                await user.updateOne({ _id: user.id }, {
-                    set: { token },
-                })
-                user.save()
-                next();
-            })
-            return res.status(200).json({ user });
-        }
+
+            await user.updateOne({ _id: user.id }, { $set: { token } });
+            return res.status(200).json({ user, token });
+        });
     } catch (err) {
         console.log(err);
         res.status(500).send('An error occurred during login');
     }
 });
+
 
 app.listen(port, () => {
     console.log(port, "Server is running on port 8000");
